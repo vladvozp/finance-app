47932947dd162ec310148d82e08cfbe378ce4b95
vladvozp <vladvozp@gmail.com>
2025-09-08 14:58:24 +0200

    Protected Route

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -3,14 +3,18 @@ import Layout from "../components/Layout.jsx";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login.jsx";
 import Guest from "../pages/Guest.jsx";
+import ProtectedRoute from "../app/ProtectedRoute.jsx"
 
 export const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/login", element: <Login /> },
-      { path: "/guest", element: <Guest /> },
+      // Protected
+      { path: "/", element: <ProtectedRoute><Home /></ProtectedRoute> },
+      
+      // Public
+      { path: "/login", element: <Login />},
+      { path: "/guest", element: <Guest />},
     ],
   },
 ]);
